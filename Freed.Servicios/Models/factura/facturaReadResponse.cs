8015631d596626b768ab3a309cc0c67b470dbb9d
Gregorio Castro <josegregorio994@gmail.com>
2017-03-25 18:31:08 -0400

    Se empezo con factura

---
+++ Freed.Servicios/Models/factura/facturaReadResponse.cs
@@ -0,0 +1,34 @@
+﻿using Freed.Servicios.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Freed.Servicios.Models.factura
+{
+    [DataContract]
+    public class facturaReadResponse
+    {
+        [DataMember]
+        public int code { get; set; }
+
+        [DataMember]
+        public String messageDetail { get; set; }
+
+        [DataMember]
+        public String messageException { get; set; }
+
+        [DataMember]
+        public facturaDTO data { get; set; }
+
+        public facturaReadResponse(int code, String messageDetail, String messageException, facturaDTO data)
+        {
+            this.code = code;
+            this.messageDetail = messageDetail;
+            this.messageException = messageException;
+            this.data = data;
+        }
+
+    }
+}
