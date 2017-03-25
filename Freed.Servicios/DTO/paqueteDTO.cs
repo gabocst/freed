54801d3107fc7a7430c1d4d4c726297724e9a344
Jose Gabriel Castro <jose.castro.lazo@gmail.com>
2017-03-25 18:44:53 -0400

    dto y model de paquete

---
+++ Freed.Servicios/DTO/paqueteDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Freed.Servicios.DTO
+{
+    [DataContract]
+    public class paqueteDTO
+    {
+        [DataMember]
+        public int id { get; set; }
+
+        [DataMember]
+        public System.DateTime fechaCreacion { get; set; }
+
+        [DataMember]
+        public string nombre { get; set; }
+    }
+}
---
+++ Freed.Servicios/Models/paquete/paqueteListResponse.cs
@@ -0,0 +1,33 @@
+﻿using Freed.Servicios.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Freed.Servicios.Models.paquete
+{
+    [DataContract]
+    public class paqueteListResponse
+    {
+        [DataMember]
+        public int code { get; set; }
+
+        [DataMember]
+        public String messageDetail { get; set; }
+
+        [DataMember]
+        public String messageException { get; set; }
+
+        [DataMember]
+        public List<paqueteDTO> data { get; set; }
+
+        public paqueteListResponse(int code, String messageDetail, String messageException, List<paqueteDTO> data)
+        {
+            this.code = code;
+            this.messageDetail = messageDetail;
+            this.messageException = messageException;
+            this.data = data;
+        }
+    }
+}
